51e2527dfcc4152cdc3d51c0f78a4b50b4b8f208
ming0209 <66905951+ming0209@users.noreply.github.com>
2022-07-01 13:42:51 +0800

    Initial commit

---
+++ src/main/java/com/ming0209/magiccreation/main.java
@@ -0,0 +1,17 @@
+package com.ming0209.magiccreation;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
